87843742f49614b97756ed4344d1eee8aad30d3b
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-12-24 20:35:46 +0530

    Fixed bug in hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Hello, I'm{" "}</span>
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Hello, I&apos;m{" "}</span>
                 <br></br>
                 <TypeAnimation
                     sequence={[
@@ -42,7 +42,7 @@ const HeroSection = () => {
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
                     <Image 
-                    src="/images/hero-image.png"
+                    src="/hero-image.png"
                     className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                     alt="hero image" 
                     width={300} 
