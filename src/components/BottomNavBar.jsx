067f93ab7184ebcf0386582770677a95b2ba7011
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-12-25 10:48:23 +0530

    Fixed bottom navbar size

--- src/components/BottomNavBar.jsx
+++ src/components/BottomNavBar.jsx
@@ -23,7 +23,7 @@ const BottomNavBar = () => {
     return (
         <nav className="fixed bottom-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100 border-t border-white">
             <div className="menu block sm:hidden md:w-auto" id="navbar">
-                <ul className="flex justify-evenly p-4 md:p-0 md:flex-row md:space-x-8 mt-0 ">
+                <ul className="flex justify-evenly p-2 md:p-0 md:flex-row md:space-x-8 mt-0 ">
                         {
                             navLinks.map((link, index) => (
                                 <li key={index}>
